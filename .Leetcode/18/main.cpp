ad46b0d8313d5f78d48367d893d0144b61387544
LeeMjnnkDzuy <duylelv17@gmail.com>
2024-12-16 15:56:51 +0700

    1

---
+++ .Leetcode/18/main.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define FI "input.INP"
+using namespace std;
+
+void run_debug(bool check){
+	if (check)
+	{
+		freopen(FI, "r", stdin);
+	}
+}
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
+	int a, b, c, d;
+	for (int i = 0; i < nums.size() - 3; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+		a = nums[i];
+		for (int j = i + 1; j < nums.size() - 2; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            
+            int left = j + 1;
+            int right = nums.size() - 1;
+            b = nums[j];
+            while (left < right) {
+
+				c = nums[left];
+				d = nums[right];
+                int sum = a + b + c + d;
+                
+                if (sum == target) {
+                    res.push_back({a, b, d, c});
+                    
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+    
+    return res;
+}
+
+
+void func(){
+	int target, a;
+	vector<int> nums;
+	cin >> target;
+	while (cin >> a)
+	{
+		nums.push_back(a);
+	}
+
+	cout << target << endl;
+
+	for (int i = 0; i < nums.size(); i++){
+		cout << nums[i] << " ";
+	}
+	cout << endl;
+
+	vector<vector<int>> temp = fourSum(nums, target);
+
+	for (int i = 0; i < temp.size(); i++)
+	{
+		for (int j = 0; j < temp[i].size(); j++)
+		{
+			cout << temp[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+	run_debug(true);
+	func();
+	return 0;
+}
